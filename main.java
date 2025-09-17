6821f4dc9bbdb3d89ffa0e43cb580751250cb690
swayammankar <2024.swayam.mankar@ves.ac.in>
2025-09-17 09:57:23 +0530

    Create main.java

---
+++ main.java
@@ -0,0 +1,14 @@
+public class Addition {
+    public static void main(String[] args) {
+        // Print a greeting
+        System.out.println("Hello, Java world!");
+
+        // Declare two integers
+        int a = 5;
+        int b = 7;
+
+        // Add them and print the result
+        int sum = a + b;
+        System.out.println("The sum of " + a + " and " + b + " is: " + sum);
+    }
+}
